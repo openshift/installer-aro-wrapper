22d8b389ff6a33107332b5a94f9d0837c1f9a491
Jim Minter <jminter@redhat.com>
2019-12-30 11:18:23 -0600

    move pkg/util/azureclient to pkg/util/azureclient/mgmt

---
+++ pkg/util/azureclient/mgmt/network/publicipaddresses.go
@@ -0,0 +1,33 @@
+package network
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-07-01/network"
+	"github.com/Azure/go-autorest/autorest"
+)
+
+// PublicIPAddressesClient is a minimal interface for azure PublicIPAddressesClient
+type PublicIPAddressesClient interface {
+	List(ctx context.Context, resourceGroupName string) (ips []network.PublicIPAddress, err error)
+	PublicIPAddressesClientAddons
+}
+
+type publicIPAddressesClient struct {
+	network.PublicIPAddressesClient
+}
+
+var _ PublicIPAddressesClient = &publicIPAddressesClient{}
+
+// NewPublicIPAddressesClient creates a new PublicIPAddressesClient
+func NewPublicIPAddressesClient(subscriptionID string, authorizer autorest.Authorizer) PublicIPAddressesClient {
+	client := network.NewPublicIPAddressesClient(subscriptionID)
+	client.Authorizer = authorizer
+
+	return &publicIPAddressesClient{
+		PublicIPAddressesClient: client,
+	}
+}
