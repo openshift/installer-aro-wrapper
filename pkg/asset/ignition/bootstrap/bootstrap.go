540fe7914d54f8e1f1cdbc1b0c40c6e29008c23d
rna-afk <anarayan@redhat.com>
2024-07-26 15:22:10 -0400

    aro: Bring all the fork work to wrapper
    Bringing all the files from the ARO fork of the installer to the
    ARO wrapper code and added base code for ignition command run.

---
+++ pkg/asset/ignition/bootstrap/bootstrap.go
@@ -0,0 +1,39 @@
+package bootstrap
+
+import (
+	"github.com/openshift/installer/pkg/asset"
+)
+
+const (
+	bootstrapIgnFilename = "bootstrap.ign"
+)
+
+// Bootstrap is an asset that generates the ignition config for bootstrap nodes.
+type Bootstrap struct {
+	Common
+}
+
+var _ asset.WritableAsset = (*Bootstrap)(nil)
+
+// Generate generates the ignition config for the Bootstrap asset.
+func (a *Bootstrap) Generate(dependencies asset.Parents) error {
+	templateData := a.getTemplateData(dependencies, false)
+	if err := a.generateConfig(dependencies, templateData); err != nil {
+		return err
+	}
+
+	if err := a.generateFile(bootstrapIgnFilename); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Name returns the human-friendly name of the asset.
+func (a *Bootstrap) Name() string {
+	return "Bootstrap Ignition Config"
+}
+
+// Load returns the bootstrap ignition from disk.
+func (a *Bootstrap) Load(f asset.FileFetcher) (found bool, err error) {
+	return a.load(f, bootstrapIgnFilename)
+}
---
+++ pkg/installer/customignition.go
@@ -0,0 +1,22 @@
+package installer
+
+import (
+	"context"
+
+	"github.com/openshift/installer-aro-wrapper/pkg/asset/targets"
+	"github.com/openshift/installer-aro-wrapper/pkg/cluster/graph"
+)
+
+// Add new ARO assets
+// Create the ignition assets (targets)
+func (m *manager) InjectIgnitionFiles(ctx context.Context, g graph.Graph) (graph.Graph, error) {
+	// Resolve the asset graph.
+	m.log.Print("resolving graph")
+	for _, a := range targets.Cluster {
+		err := g.Resolve(a)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return g, nil
+}
