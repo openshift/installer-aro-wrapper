21dbdc80a9913dd9e6fb9ca4f9c7a11a974a2f8c
mrWinston <maschulz@redhat.com>
2024-08-13 16:51:19 +0200

    clarify logging and comments

--- hack/update-go-module-dependencies.sh
+++ hack/update-go-module-dependencies.sh
@@ -26,7 +26,6 @@
 # with github.com/openshift/cluster-api-provider-azure (just an example, there are more).
 
 for x in vendor/github.com/openshift/*; do
-  echo "adjusting for $x"
 	case $x in
 		# Review the list of special cases on each release.
 
@@ -38,23 +37,29 @@ for x in vendor/github.com/openshift/*; do
 		vendor/github.com/openshift/installer)
 			;;
     
+    # Assisted-service is actually two modules that need to be updated separately, see below
     vendor/github.com/openshift/assisted-service)
       ;;
+
+    # we need a custom replace directive to replace sig.k8s.io/cluster-api with the openshift-version.
+    # Same with ibmcloud and libvirt api-provider. see below
     vendor/github.com/openshift/cluster-api)
       ;;
+		vendor/github.com/openshift/cluster-api-provider-ibmcloud)
+			;;
+		vendor/github.com/openshift/cluster-api-provider-libvirt)
+			;;
+
 		# Inconsistent imports: some of our dependencies import it as github.com/metal3-io/cluster-api-provider-baremetal
 		# but in some places directly from the openshift fork.
 		# Replace github.com/metal3-io/cluster-api-provider-baremetal with an openshift fork in go.mod
 		vendor/github.com/openshift/cluster-api-provider-baremetal)
 			;;
 
-		vendor/github.com/openshift/cluster-api-provider-ibmcloud)
-			;;
-		vendor/github.com/openshift/cluster-api-provider-libvirt)
-			;;
 		# It is only used indirectly and intermediate dependencies pin to different incompatible commits.
 		# We force a specific commit here to make all dependencies happy.
 		vendor/github.com/openshift/cloud-credential-operator)
+      echo "pinning $x"
 			go mod edit -replace github.com/openshift/cloud-credential-operator=github.com/openshift/cloud-credential-operator@v0.0.0-20200316201045-d10080b52c9e
 			;;
 
@@ -64,24 +69,27 @@ for x in vendor/github.com/openshift/*; do
 			;;
 
 		*)
+      echo "updating $x"
 			go mod edit -replace ${x##vendor/}=$(go list -mod=mod -m ${x##vendor/}@release-4.12 | sed -e 's/ /@/')
 			;;
 	esac
 done
 
-echo "Doing apiproviders"
 for x in aws azure openstack ibmcloud libvirt; do
+  echo "updating apiprovider $x"
 	go mod edit -replace sigs.k8s.io/cluster-api-provider-$x=$(go list -mod=mod -m github.com/openshift/cluster-api-provider-$x@release-4.12 | sed -e 's/ /@/')
 done
 
-echo "Doing clusterapi"
+echo "updating sigs.k8s.io/cluster-api with openshift fork"
 go mod edit -replace sigs.k8s.io/cluster-api=$(go list -mod=mod -m github.com/openshift/cluster-api@release-4.12 | sed -e 's/ /@/')
 
-echo "Doing installer"
+echo "updating installer with aro fork"
 go mod edit -replace github.com/openshift/installer=$(go list -mod=mod -m github.com/openshift/installer-aro@release-4.12-azure | sed -e 's/ /@/')
 
-echo "Doing assisted service"
+echo "updating openshift/assisted-service/api"
 go mod edit -replace github.com/openshift/assisted-service/api=$(go list -mod=mod -m github.com/openshift/assisted-service/api@release-4.12 | sed -e 's/ /@/')
+
+echo "updating openshift/assisted-service/models"
 go mod edit -replace github.com/openshift/assisted-service/models=$(go list -mod=mod -m github.com/openshift/assisted-service/models@release-4.12 | sed -e 's/ /@/')
 
 echo "updating rest"
