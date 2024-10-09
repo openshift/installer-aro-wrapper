3216c90313b49951782921fa47d71e81934b817f
Rajdeep Singh Chauhan <rajdchau@redhat.com>
2024-10-09 14:26:01 -0400

    ARO-9712 use user delegated SAS tokens for ignition file pull

--- pkg/util/mocks/graph/graph.go
+++ pkg/util/mocks/graph/graph.go
@@ -51,6 +51,21 @@ func (mr *MockManagerMockRecorder) Exists(arg0, arg1, arg2 interface{}) *gomock.
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Exists", reflect.TypeOf((*MockManager)(nil).Exists), arg0, arg1, arg2)
 }
 
+// GetUserDelegatedSASIgnitionBlobURL mocks base method.
+func (m *MockManager) GetUserDelegatedSASIgnitionBlobURL(arg0 context.Context, arg1, arg2, arg3 string, arg4 bool) (string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetUserDelegatedSASIgnitionBlobURL", arg0, arg1, arg2, arg3, arg4)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetUserDelegatedSASIgnitionBlobURL indicates an expected call of GetUserDelegatedSASIgnitionBlobURL.
+func (mr *MockManagerMockRecorder) GetUserDelegatedSASIgnitionBlobURL(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetUserDelegatedSASIgnitionBlobURL", reflect.TypeOf((*MockManager)(nil).GetUserDelegatedSASIgnitionBlobURL), arg0, arg1, arg2, arg3, arg4)
+}
+
 // LoadPersisted mocks base method.
 func (m *MockManager) LoadPersisted(arg0 context.Context, arg1, arg2 string) (graph.PersistedGraph, error) {
 	m.ctrl.T.Helper()
