343eaccb97bb9a384ac08c85818a2425207aba22
Tanmay Satam <tsatam@redhat.com>
2024-10-11 11:34:14 -0400

    Add utilpem.Encode function to write certificates

--- pkg/util/pem/pem.go
+++ pkg/util/pem/pem.go
@@ -8,7 +8,12 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
 )
+
+type Encodable interface {
+	*x509.Certificate | *x509.CertificateRequest | *rsa.PublicKey | *rsa.PrivateKey
+}
 
 func Parse(b []byte) (key *rsa.PrivateKey, certs []*x509.Certificate, err error) {
 	for {
@@ -49,4 +54,39 @@ func Parse(b []byte) (key *rsa.PrivateKey, certs []*x509.Certificate, err error)
 	}
 
 	return key, certs, nil
+}
+
+func Encode[V Encodable](w io.Writer, inputs ...V) (err error) {
+	for _, cer := range inputs {
+		pemType := "UNKNOWN"
+		var rawBytes []byte
+
+		switch t := any(cer).(type) {
+		case *x509.Certificate:
+			pemType = "CERTIFICATE"
+			rawBytes = t.Raw
+		case *x509.CertificateRequest:
+			pemType = "CERTIFICATE REQUEST"
+			rawBytes = t.Raw
+		case *rsa.PrivateKey:
+			pemType = "RSA PRIVATE KEY"
+			rawBytes = x509.MarshalPKCS1PrivateKey(t)
+		case *rsa.PublicKey:
+			pemType = "RSA PUBLIC KEY"
+			rawBytes, err = x509.MarshalPKIXPublicKey(t)
+			if err != nil {
+				return
+			}
+		default:
+			err = fmt.Errorf("unable to identify %v", t)
+			return
+		}
+
+		err = pem.Encode(w, &pem.Block{Type: pemType, Bytes: rawBytes})
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
