3edf80982bd4bc2018062d899f56ba16dcc77e8d
Tanmay Satam <tsatam@redhat.com>
2024-11-07 14:16:17 -0500

    Use managed boot diagnostics for bootstrap and master VMs

--- pkg/installer/deployresources_resources.go
+++ pkg/installer/deployresources_resources.go
@@ -150,8 +150,7 @@ func (m *manager) computeBootstrapVM(installConfig *installconfig.InstallConfig)
 			},
 			DiagnosticsProfile: &mgmtcompute.DiagnosticsProfile{
 				BootDiagnostics: &mgmtcompute.BootDiagnostics{
-					Enabled:    to.BoolPtr(true),
-					StorageURI: to.StringPtr("https://cluster" + m.oc.Properties.StorageSuffix + ".blob." + m.env.Environment().StorageEndpointSuffix + "/"),
+					Enabled: to.BoolPtr(true),
 				},
 			},
 		},
@@ -222,8 +221,7 @@ func (m *manager) computeMasterVMs(installConfig *installconfig.InstallConfig, z
 			},
 			DiagnosticsProfile: &mgmtcompute.DiagnosticsProfile{
 				BootDiagnostics: &mgmtcompute.BootDiagnostics{
-					Enabled:    to.BoolPtr(true),
-					StorageURI: to.StringPtr("https://cluster" + m.oc.Properties.StorageSuffix + ".blob." + m.env.Environment().StorageEndpointSuffix + "/"),
+					Enabled: to.BoolPtr(true),
 				},
 			},
 		},
