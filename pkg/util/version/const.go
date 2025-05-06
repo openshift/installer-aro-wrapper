029b90075682f13bf09bbe441b13d8740589fefc
Preetish Kumar Tripathi <11624434+preetisht@users.noreply.github.com>
2025-05-06 13:56:37 +0530

    Made changes to point to the latest MDM and MDSD images

--- pkg/util/version/const.go
+++ pkg/util/version/const.go
@@ -22,8 +22,19 @@ func FluentbitImage(acrDomain string) string {
 	return acrDomain + "/fluentbit:1.9.10-cm20241208@sha256:fa35a491542b1e531b73658da83e47f0f549786a186f00b0cdaffec86100c980"
 }
 
+// MdmImage contains the location of the MDM container image
+// https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
+func MdmImage(acrDomain string) string {
+	return acrDomain + "/distroless/genevamdm:2.2025.404.1254-77220c-20250406t1133@sha256:8e89bfec19c81398afa0ec51a97d748cc6b7b85cf9440dd1c7ea75b24302fe55"
+}
+
 // MdsdImage contains the location of the MDSD container image
 // https://eng.ms/docs/products/geneva/collect/references/linuxcontainers
 func MdsdImage(acrDomain string) string {
-	return acrDomain + "/distroless/genevamdsd:mariner_20241212.2@sha256:a7a71af5b631ea5a8ad587a09d8680b17719cae25b95de81e8a4d71f2cc55f45"
+	return acrDomain + "/distroless/genevamdsd:mariner_20250225.2@sha256:da59ef7cfe3b0b9b6b453930cc629605cf3528ed11dbb88cdc50a38633198add"
+}
+
+// MUOImage contains the location of the Managed Upgrade Operator container image
+func MUOImage(acrDomain string) string {
+	return acrDomain + "/managed-upgrade-operator:aro-b4"
 }
