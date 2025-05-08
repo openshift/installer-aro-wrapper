c18e0b87371a59ec6d4049733b99aafb3224c341
Amber Brown <ambrown@redhat.com>
2025-05-08 14:18:04 +1000

    update how we handle zones, including handling 4 AZs, handling 3 non-consecutive AZs, and other such fun

--- pkg/installer/generateconfig_test.go
+++ pkg/installer/generateconfig_test.go
@@ -4,10 +4,12 @@ package installer
 // Licensed under the Apache License 2.0.
 
 import (
+	"reflect"
 	"testing"
 
 	mgmtcompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
 	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/google/go-cmp/cmp"
 	"github.com/openshift/installer/pkg/types/azure"
 )
 
@@ -41,6 +43,92 @@ func TestVMNetworkingType(t *testing.T) {
 
 			if result != tt.wantType {
 				t.Error(result)
+			}
+		})
+	}
+}
+
+func TestDetermineZones(t *testing.T) {
+	for _, tt := range []struct {
+		name                  string
+		controlPlaneSkuZones  []string
+		workerSkuZones        []string
+		wantControlPlaneZones []string
+		wantWorkerZones       []string
+		wantErr               string
+	}{
+
+		{
+			name:                  "non-zonal control plane, zonal workers",
+			controlPlaneSkuZones:  nil,
+			workerSkuZones:        []string{"1", "2", "3"},
+			wantControlPlaneZones: []string{""},
+			wantWorkerZones:       []string{"1", "2", "3"},
+		},
+		{
+			name:                  "zonal control plane, non-zonal workers",
+			controlPlaneSkuZones:  []string{"1", "2", "3"},
+			workerSkuZones:        nil,
+			wantControlPlaneZones: []string{"1", "2", "3"},
+			wantWorkerZones:       []string{""},
+		},
+		{
+			name:                  "zonal control plane, zonal workers",
+			controlPlaneSkuZones:  []string{"1", "2", "3"},
+			workerSkuZones:        []string{"1", "2", "3"},
+			wantControlPlaneZones: []string{"1", "2", "3"},
+			wantWorkerZones:       []string{"1", "2", "3"},
+		},
+		{
+			name:                  "region with 4 availability zones, control plane uses first 3, workers use all",
+			controlPlaneSkuZones:  []string{"1", "2", "3", "4"},
+			workerSkuZones:        []string{"1", "2", "3", "4"},
+			wantControlPlaneZones: []string{"1", "2", "3"},
+			wantWorkerZones:       []string{"1", "2", "3", "4"},
+		},
+		{
+			name:                 "not enough control plane zones",
+			controlPlaneSkuZones: []string{"1", "2"},
+			workerSkuZones:       []string{"1", "2", "3"},
+			wantErr:              "cluster creation with 2 zones and 3 control plane replicas is unsupported",
+		},
+		{
+			name:                 "not enough worker zones",
+			controlPlaneSkuZones: []string{"1", "2", "3"},
+			workerSkuZones:       []string{"1", "2"},
+			wantErr:              "cluster creation with a worker SKU available on less than 3 zones is unsupported (available: 2)",
+		},
+		{
+			name:                  "region with 4 availability zones, control plane only available in non-consecutive 3, workers use all",
+			controlPlaneSkuZones:  []string{"1", "2", "4"},
+			workerSkuZones:        []string{"1", "2", "3", "4"},
+			wantControlPlaneZones: []string{"1", "2", "4"},
+			wantWorkerZones:       []string{"1", "2", "3", "4"},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			controlPlaneSku := &mgmtcompute.ResourceSku{
+				LocationInfo: &[]mgmtcompute.ResourceSkuLocationInfo{
+					{Zones: &tt.controlPlaneSkuZones},
+				},
+			}
+			workerSku := &mgmtcompute.ResourceSku{
+				LocationInfo: &[]mgmtcompute.ResourceSkuLocationInfo{
+					{Zones: &tt.workerSkuZones},
+				},
+			}
+
+			controlPlaneZones, workerZones, err := determineAvailabilityZones(controlPlaneSku, workerSku)
+			if err != nil && err.Error() != tt.wantErr {
+				t.Error(cmp.Diff(err, tt.wantErr))
+			}
+
+			if !reflect.DeepEqual(controlPlaneZones, tt.wantControlPlaneZones) {
+				t.Error(cmp.Diff(controlPlaneZones, tt.wantControlPlaneZones))
+			}
+
+			if !reflect.DeepEqual(workerZones, tt.wantWorkerZones) {
+				t.Error(cmp.Diff(workerZones, tt.wantWorkerZones))
 			}
 		})
 	}
