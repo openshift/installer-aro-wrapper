334304bad67d49e7b1a8d7fe1320feac9fa68392
Amber Brown <ambrown@redhat.com>
2025-09-24 12:03:40 +1000

    update to release-4.19 in script

--- hack/update-go-module-dependencies.sh
+++ hack/update-go-module-dependencies.sh
@@ -3,7 +3,7 @@
 export GOPRIVATE=github.com
 export GONOPROXY="y"
 
-RELEASE=release-4.18
+RELEASE=release-4.19
 VM_SKU=aro_4$(echo $RELEASE | sed 's/.*\.//')
 declare -a pinned=(
 	"github.com/openshift/assisted-service/api"
