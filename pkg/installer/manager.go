597ca30b2686e1bfbe5d3c16d0b7457e0eee188c
Amber Brown <ambrown@redhat.com>
2026-04-30 15:17:17 +1000

    update to use the resourceskus in the cx sub

--- pkg/installer/manager.go
+++ pkg/installer/manager.go
@@ -14,6 +14,7 @@ import (
 	"github.com/openshift/installer-aro-wrapper/pkg/bootstraplogging"
 	"github.com/openshift/installer-aro-wrapper/pkg/cluster/graph"
 	"github.com/openshift/installer-aro-wrapper/pkg/env"
+	"github.com/openshift/installer-aro-wrapper/pkg/util/azureclient/azuresdk/armcompute"
 	"github.com/openshift/installer-aro-wrapper/pkg/util/azureclient/mgmt/features"
 	"github.com/openshift/installer-aro-wrapper/pkg/util/refreshable"
 )
@@ -31,7 +32,8 @@ type manager struct {
 	sub          *api.Subscription
 	fpAuthorizer refreshable.Authorizer
 
-	deployments features.DeploymentsClient
+	deployments     features.DeploymentsClient
+	armResourceSKUs armcompute.ResourceSKUsClient
 
 	graph graph.Manager
 
@@ -50,7 +52,7 @@ func gatewayDomains(env env.Interface, oc *api.OpenShiftCluster) []string {
 	return append(env.GatewayDomains(), oc.Properties.ImageRegistryStorageAccountName+".blob."+env.Environment().StorageEndpointSuffix)
 }
 
-func NewInstaller(log *logrus.Entry, _env env.Interface, assetsDir string, clusterUUID string, oc *api.OpenShiftCluster, subscription *api.Subscription, fpAuthorizer refreshable.Authorizer, deployments features.DeploymentsClient, g graph.Manager) Interface {
+func NewInstaller(log *logrus.Entry, _env env.Interface, assetsDir string, clusterUUID string, oc *api.OpenShiftCluster, subscription *api.Subscription, fpAuthorizer refreshable.Authorizer, deployments features.DeploymentsClient, armResourceSKUs armcompute.ResourceSKUsClient, g graph.Manager) Interface {
 	return &manager{
 		log:                       log,
 		env:                       _env,
@@ -59,6 +61,7 @@ func NewInstaller(log *logrus.Entry, _env env.Interface, assetsDir string, clust
 		oc:                        oc,
 		sub:                       subscription,
 		fpAuthorizer:              fpAuthorizer,
+		armResourceSKUs:           armResourceSKUs,
 		deployments:               deployments,
 		graph:                     g,
 		getBootstrapLoggingConfig: bootstraplogging.GetConfig,
