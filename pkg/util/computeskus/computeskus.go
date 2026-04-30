4a745bd32b93c762b74240324a4be4a4ab61c839
Amber Brown <ambrown@redhat.com>
2026-04-30 13:57:27 +1000

    move over the computeskus/resourceskus code from ARO-RP

--- pkg/util/computeskus/computeskus.go
+++ pkg/util/computeskus/computeskus.go
@@ -4,29 +4,47 @@ package computeskus
 // Licensed under the Apache License 2.0.
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
 	"strings"
 
-	mgmtcompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
+	sdkcompute "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v7"
+
+	"github.com/openshift/installer-aro-wrapper/pkg/util/azureclient/azuresdk/armcompute"
 )
 
+const (
+	standardDisk          = "StandardSSD_LRS"
+	premiumDisk           = "Premium_LRS"
+	premiumDiskCapability = "PremiumIO"
+)
+
+var ErrListVMResourceSKUs = errors.New("failure listing resource SKUs")
+
 // Zones returns zone information for the resource SKU
-func Zones(sku *mgmtcompute.ResourceSku) []string {
-	if sku.LocationInfo == nil ||
-		len(*sku.LocationInfo) == 0 ||
-		(*sku.LocationInfo)[0].Zones == nil {
+func Zones(sku *sdkcompute.ResourceSKU) []string {
+	if len(sku.LocationInfo) == 0 ||
+		sku.LocationInfo[0] == nil ||
+		(sku.LocationInfo)[0].Zones == nil {
 		return nil
 	}
 
-	return *(*sku.LocationInfo)[0].Zones
+	r := []string{}
+	for _, x := range sku.LocationInfo[0].Zones {
+		r = append(r, *x)
+	}
+	return r
 }
 
 // HasCapability checks whether given resource SKU has specific capability
-func HasCapability(sku *mgmtcompute.ResourceSku, capabilityName string) bool {
+func HasCapability(sku *sdkcompute.ResourceSKU, capabilityName string) bool {
 	if sku.Capabilities == nil {
 		return false
 	}
 
-	for _, c := range *sku.Capabilities {
+	for _, c := range sku.Capabilities {
 		if *c.Name == capabilityName {
 			return *c.Value == "True"
 		}
@@ -36,11 +54,13 @@ func HasCapability(sku *mgmtcompute.ResourceSku, capabilityName string) bool {
 }
 
 // IsRestricted checks whether given resource SKU is restricted in a given location
-func IsRestricted(skus map[string]*mgmtcompute.ResourceSku, location, VMSize string) bool {
-	for _, restriction := range *skus[VMSize].Restrictions {
-		for _, restrictedLocation := range *restriction.RestrictionInfo.Locations {
-			if restrictedLocation == location {
-				return true
+func IsRestricted(sku *sdkcompute.ResourceSKU, location string) bool {
+	for _, restriction := range sku.Restrictions {
+		if restriction != nil && restriction.RestrictionInfo != nil {
+			for _, restrictedLocation := range restriction.RestrictionInfo.Locations {
+				if restrictedLocation != nil && strings.EqualFold(*restrictedLocation, location) {
+					return true
+				}
 			}
 		}
 	}
@@ -48,46 +68,84 @@ func IsRestricted(skus map[string]*mgmtcompute.ResourceSku, location, VMSize str
 	return false
 }
 
-// GetCapabilityMap converts *[]ResourceSkuCapabilities to map[string]string
-func GetCapabilityMap(sku *mgmtcompute.ResourceSku) (map[string]string, bool) {
-	if sku.Capabilities == nil || len(*sku.Capabilities) == 0 {
-		return nil, false
+// SupportedOSDisk returns the type of OSDisk for the given resource. Most VMs will use Premium disks but some SKUs only support Standard SSDs
+func SupportedOSDisk(vmSku *sdkcompute.ResourceSKU) string {
+	if HasCapability(vmSku, premiumDiskCapability) {
+		return premiumDisk
 	}
-	capabilityMap := make(map[string]string, len(*sku.Capabilities))
-	for _, c := range *sku.Capabilities {
-		if c.Name != nil && c.Value != nil {
-			capabilityMap[*c.Name] = *c.Value
-		}
-	}
-	return capabilityMap, len(capabilityMap) > 0
+	return standardDisk
 }
 
-// FilterVMSizes filters resource SKU by location and returns only virtual machines, their names, restrictions, location info, and capabilities.
-func FilterVMSizes(skus []mgmtcompute.ResourceSku, location string) map[string]*mgmtcompute.ResourceSku {
-	vmskus := map[string]*mgmtcompute.ResourceSku{}
-	for _, sku := range skus {
-		// TODO(mjudeikis): At some point some SKU's stopped returning zones and
-		// locations. IcM is open with MSFT but this might take a while.
-		// Revert once we find out right behaviour.
-		// https://github.com/openshift/ARO-Installer/issues/1515
-		if len(*sku.Locations) == 0 || !strings.EqualFold((*sku.Locations)[0], location) ||
-			*sku.ResourceType != "virtualMachines" {
+func SelectVMSkusInCurrentRegion(ctx context.Context, resourceSkusClient armcompute.ResourceSKUsClient, location string, skuNames []string) (map[string]*sdkcompute.ResourceSKU, error) {
+	// Sort and compact so that we only have one instance of each SKU in the list
+	slices.Sort(skuNames)
+	skuNames = slices.Compact(skuNames)
+
+	vmskus := map[string]*sdkcompute.ResourceSKU{}
+	filter := fmt.Sprintf("location eq %s", location)
+	skusIter := resourceSkusClient.List(ctx, filter, false)
+
+	for sku, err := range skusIter {
+		if err != nil {
+			return nil, fmt.Errorf("%w: %w", ErrListVMResourceSKUs, err)
+		}
+
+		if sku == nil || sku.ResourceType == nil || sku.Name == nil {
 			continue
 		}
 
-		if len(*sku.LocationInfo) == 0 { // happened in eastus2euap
+		// We only care about VMs and ones with locations/locationinfo
+		if *sku.ResourceType != "virtualMachines" || len(sku.Locations) == 0 || len(sku.LocationInfo) == 0 {
 			continue
 		}
 
-		// We copy only part of the object so we don't have to keep
-		// a lot of data in memory.
-		vmskus[*sku.Name] = &mgmtcompute.ResourceSku{
-			Name:         sku.Name,
-			Restrictions: sku.Restrictions,
-			LocationInfo: sku.LocationInfo,
-			Capabilities: sku.Capabilities,
+		// Make sure it's actually in our location
+		if !slices.ContainsFunc(sku.Locations, func(s *string) bool { return s != nil && strings.EqualFold(*s, location) }) {
+			continue
+		}
+
+		if slices.Contains(skuNames, *sku.Name) {
+			vmskus[*sku.Name] = sku
+		}
+
+		// If we've already found all the SKUs we want, exit
+		if len(vmskus) == len(skuNames) {
+			break
 		}
 	}
 
-	return vmskus
+	return vmskus, nil
+}
+
+func ListUnrestrictedVMSkusInCurrentRegion(ctx context.Context, resourceSkusClient armcompute.ResourceSKUsClient, location string) ([]string, error) {
+	vmskus := []string{}
+	filter := fmt.Sprintf("location eq %s", location)
+	skusIter := resourceSkusClient.List(ctx, filter, false)
+
+	for sku, err := range skusIter {
+		if err != nil {
+			return nil, fmt.Errorf("%w: %w", ErrListVMResourceSKUs, err)
+		}
+
+		if sku == nil || sku.ResourceType == nil || sku.Name == nil {
+			continue
+		}
+
+		// We only care about VMs and ones with locations/locationinfo
+		if *sku.ResourceType != "virtualMachines" || len(sku.Locations) == 0 || len(sku.LocationInfo) == 0 {
+			continue
+		}
+
+		// Make sure it's actually in our location
+		if !slices.ContainsFunc(sku.Locations, func(s *string) bool { return s != nil && strings.EqualFold(*s, location) }) {
+			continue
+		}
+
+		if !IsRestricted(sku, location) {
+			vmskus = append(vmskus, *sku.Name)
+		}
+	}
+
+	slices.Sort(vmskus)
+	return slices.Compact(vmskus), nil
 }
