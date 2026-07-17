d81ef84fda912ac37b54f977905b7ea4c7433f16
Edison Cardenas (RED HAT INC) <b-ecardenas@microsoft.com>
2026-07-17 18:34:00 +1200

    refactor: move node-image-pull ordering to aro-etchosts-resolver.service
    Instead of injecting a drop-in into node-image-pull.service from
    dnsmasq_config.go, declare Before=node-image-pull.service directly on
    aro-etchosts-resolver.service. This is cleaner since aro-etchosts-resolver
    is an ARO-owned unit and avoids modifying an upstream service via ignition.

--- pkg/installer/etchost/etchosts.go
+++ pkg/installer/etchost/etchosts.go
@@ -81,6 +81,7 @@ cmp "${TEMP_FILE}" "${HOSTS_FILE}" || cp -f "${TEMP_FILE}" "${HOSTS_FILE}"
 var aroUnitTemplate = template.Must(template.New("etchostservice").Parse(`[Unit]
 Description=One shot service that appends static domains to etchosts
 Before=network-online.target
+Before=node-image-pull.service
 
 [Service]
 # ExecStart will copy the hosts defined in /etc/hosts.d/aro.conf to /etc/hosts
